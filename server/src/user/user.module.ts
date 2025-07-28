d2c542c2cb14b9c50577c4c2aa3999df174aadc9
Ваше Имя <prevetlunatikam@gmail.com>
2025-07-28 11:07:45 +0300

    vill

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './user.model';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserService],
-  controllers: [UserController],
+  controllers: [],
   exports: [UserService],
 })
 export class UserModule {}
